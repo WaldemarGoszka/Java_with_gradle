7f46ece95dad05444911f11684bc4d75375072ef
WaldemarGoszka <goszka.waldemar@gmail.com>
2022-07-04 23:46:36 +0200

    from j2900 zadania

---
+++ enumlearning/interf.java
@@ -0,0 +1,21 @@
+package enumlearning;
+
+public class interf {
+    public static String sad = "a";
+
+    static {
+        sad += "s";
+    }
+    {
+
+        sad += "n";
+    }
+    ps
+    public static void main(String[] args) {
+        System.out.println(interf.sad);
+        System.out.println(interf.sad);
+        new interf().sad;
+
+        System.out.println(interf.sad);
+    }
+}
