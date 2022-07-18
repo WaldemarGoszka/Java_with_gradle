e7b866d3f3007f6ba82113dbe13d36f1129c2cb7
WaldemarGoszka <goszka.waldemar@gmail.com>
2022-07-18 23:40:55 +0200

    from j2900 zadania

--- Lambda/LambdaExcesise.java
+++ Lambda/LambdaExcesise.java
@@ -1,5 +1,7 @@
 package Lambda;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Predicate;
 
 public class LambdaExcesise {
@@ -10,6 +12,14 @@ public class LambdaExcesise {
         LambdaInterface lambda1 = (a,b) -> a*b ;
         System.out.println(lambda1.run(5,5));
         Predicate<String> preStr  = (s) -> s.contains("sus");
-        System.out.println(preStr.test("sususususu"));
+        System.out.println(preStr.test("susu    sususu"));
+        List<String> list = new ArrayList<>();
+        list.add("gda");
+        list.add("łisx");
+        list.add("wars");
+        list.removeIf( (String s) ->  s.contains("a")) ;
+        LambdaInterface.run(10,11);
+
     }
+
 }
