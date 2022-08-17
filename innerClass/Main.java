fb9e7ee94533d0b9217a0d502061922270e1f7cf
WaldemarGoszka <goszka.waldemar@gmail.com>
2022-08-17 23:21:18 +0200

    java learning

--- innerClass/Main.java
+++ innerClass/Main.java
@@ -7,6 +7,6 @@ import java.util.List;
 public class Main {
     flat = new Flat(BigDecimal.TEN);
     List<String> strings = new ArrayList<>()
-    BlockOfFlats.Flat flat = new BlockOfFlhlats(List.of(Fl
+    BlockOfFlats.Flat flat = new BlockOfFl hlats(List.of(Fl
             ats));
 }
