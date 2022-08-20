cc2327c1a90eadb9e585a0d55ac9addd14c7523d
WaldemarGoszka <goszka.waldemar@gmail.com>
2022-08-20 23:47:08 +0200

    java learning

---
+++ innerClass/Quiz/p8/OuterClass.java
@@ -0,0 +1,18 @@
+package innerClass.Quiz.p8;
+
+public class OuterClass {
+    static class InnerClass{
+
+    }
+
+    public static void main(String[] args) {
+//        InnerClass in = new InnerClass();
+//        InnerClass in = OuterClass.new InnerClass();
+//        OuterClass.InnerClass in = new OuterClass.InnerClass();
+//        OuterClass.InnerClass in = new OuterClass().InnerClass();
+//        OuterClass.InnerClass in = new OuterClass.new InnerClass();
+//        OuterClass.InnerClass in = new OuterClass().new InnerClass();
+//        OuterClass.InnerClass in = OuterClass(). new InnerClass();
+         final String value;
+    }
+}
