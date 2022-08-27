a136082c311f68541fcc06d7d7572c78a7cb4c57
Waldemar Goszka Athlon <goszka.waldemar@gmail.com>
2022-08-27 18:33:32 +0200

    java-learning

---
+++ ExcesidesFromPDF/Conditional/Ex3.java
@@ -0,0 +1,13 @@
+package ExcesidesFromPDF.Conditional;
+
+public class Ex3 {
+
+    public static void main(String[] args) {
+        int giveMeANumber = 3;
+        if (giveMeANumber % 2 == 0){
+            System.out.println("Number is even");//pażysta
+        } else {
+            System.out.println("Number is odd");
+        }
+    }
+}
--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
-
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args ) {
-
+        int[] arr = {5,6,7};
+        System.out.println(arr);
     }
 
 
