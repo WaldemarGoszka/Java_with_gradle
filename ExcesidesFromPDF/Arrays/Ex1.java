e67acfae59fb89054353bda8acdef49a3f98335c
Waldemar Goszka Athlon <goszka.waldemar@gmail.com>
2022-08-29 20:51:40 +0200

    java-learning

--- ExcesidesFromPDF/Arrays/Ex1.java
+++ ExcesidesFromPDF/Arrays/Ex1.java
@@ -1,4 +1,7 @@
 package ExcesidesFromPDF.Arrays;
 
 public class Ex1 {
+    public static void main(String[] args) {
+
+    }
 }
--- ExcesidesFromPDF/Loops/Ex6.java
+++ ExcesidesFromPDF/Loops/Ex6.java
@@ -2,6 +2,16 @@ package ExcesidesFromPDF.Loops;
 
 public class Ex6 {
     public static void main(String[] args) {
+    int loops = 9;
+    int count = 0;
+        for (int i = 39, j = 11; count < loops ; count++) {
+            System.out.print(i+", "+j);
+            if(count != loops-1){
+                System.out.print(", ");
+            }
+            i+=2;
+            j-=3;
 
+        }
     }
 }
