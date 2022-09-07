a789ac98fb5e7398321e0cfa7300bb72fd1d4e8f
WaldemarGoszka <goszka.waldemar@gmail.com>
2022-09-07 19:54:55 +0200

    java learning

---
+++ w1/Comparable/Dog.java
@@ -0,0 +1,25 @@
+package w1.Comparable;
+
+public class Dog implements Comparable<Dog>{
+    private Integer id;
+    private String name;
+
+    public Dog(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog: " + name + "_"+ id ;
+    }
+
+    @Override
+    public int compareTo(Dog o) {
+        int result = this.name.compareTo(o.name);
+        if(result !=0){
+            return result;
+        }
+        return this.id - o.id;
+    }
+}
