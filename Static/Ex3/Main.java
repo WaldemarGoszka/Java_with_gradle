1932381ca0c7bc9b7684abd77dd59802caa5022c
WaldemarGoszka <goszka.waldemar@gmail.com>
2022-09-13 12:33:08 +0200

    java learning

--- Static/Ex3/Main.java
+++ Static/Ex3/Main.java
@@ -6,7 +6,7 @@ public class Main {
         AParent aParent1 = new Child();
         System.out.println(aParent1.getName()); // metoda niestatyczna polimorfizm
         System.out.println(aParent1.getClassName()); // nie powinno się tak wywoływać
-        System.out.println(AParent.getClassName()); // tylko tak
+        System.out.println(AParent.getClassName()); // tylko  tak
         Child child = new Child();
         System.out.println(child.getName());
         System.out.println(child.getClassName()); // tu następuje hidding
