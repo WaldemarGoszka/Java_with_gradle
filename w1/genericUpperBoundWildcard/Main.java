4ce69a763652fef9dce6d78ac4f8938103e080e4
WaldemarGoszka <goszka.waldemar@gmail.com>
2022-09-18 20:56:38 +0200

    java learning

--- w1/genericUpperBoundWildcard/Main.java
+++ w1/genericUpperBoundWildcard/Main.java
@@ -7,19 +7,34 @@ public class Main {
     public static void main(String[] args) {
         List<Animal> animals = new ArrayList<>();
         List<Dog> dogs = new ArrayList<>();
+        dogs.add(new Dog());
+        dogs.add(new Dog());
+        dogs.add(new Dog());
         List<Monkey> monkeys = new ArrayList<>();
         List<String> strings = new ArrayList<>();
+        List<FlyingDog> flyingDogs = new ArrayList<>();
 
-        List<? extends Animal> abc = monkeys;
-        abc.add(new );
+        List<? extends Dog> abc = dogs;
+        System.out.println(abc);
+        print(abc);
+        System.out.println("---");
+        System.out.println(abc);
+        System.out.println(dogs);
 //         List<? extends Animal> abc = new ArrayList<Dog>();
-        print(animals);
-        print(dogs);
-        print(monkeys);
+//        print(animals);
+//        print(flyingDogs);
+//        animals.add(new Object());
+//        print(dogs);
+//
+//        print(monkeys);
 //        print(strings);
+//        print(new Object());
     }
 
-    private static void print(List<? extends Voiceable> list) {
+    private static void print(List<? extends Dog > list) {
+//        list.add(new FlyingDog());
+        list.remove(0);
+//        list.add(new Animal());
         for (Object o : list) {
             System.out.println(o);
         }
@@ -33,6 +48,9 @@ public class Main {
 
     }
     static class Dog extends Animal{
+
+    }
+    static class FlyingDog extends Dog{
 
     }
     interface Voiceable{
--- w1/genericsEx/Ex3/Ex3.java
+++ w1/genericsEx/Ex3/Ex3.java
@@ -1,7 +1,38 @@
 package w1.genericsEx.Ex3;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Predicate;
+
 public class Ex3 {
     public static void main(String[] args) {
+        Set<Integer> set = new HashSet<>(Arrays.asList(1,2,3,4,5,6,7,8));
+        System.out.println(collectionCounter(set,(a) -> a % 2 ==0));
+
 
     }
+
+    public static <T extends Collection<E>,E, F extends Predicate<E>> int collectionCounter(T collections, F predicate){
+        int counter = 0;
+        for (E collection : collections) {
+            if(predicate.test(collection)) {
+                counter++;
+            }
+        }
+        return counter;
+
+        // 2 wersja zapisania  generyków w metodzie
+    }public static <E> int collectionCounter2(Collection<E> collections, Predicate<E> predicate){
+        int counter = 0;
+        for (E collection : collections) {
+            if(predicate.test(collection)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
 }
