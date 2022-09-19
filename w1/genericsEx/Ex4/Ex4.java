f7ff8b2c747044057073e42e5a3cdd1cb98b0588
WaldemarGoszka <goszka.waldemar@gmail.com>
2022-09-19 21:22:39 +0200

    java learning

---
+++ w1/genericsEx/Ex4/Ex4.java
@@ -0,0 +1,11 @@
+package w1.genericsEx.Ex4;
+
+public class Ex4 {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+    static <T> void changeElement ( T[] ts ){
+
+    }
+}
