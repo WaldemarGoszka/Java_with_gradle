3d70963b05b17da273d88a9917efedea79facc27
WaldemarGoszka <goszka.waldemar@gmail.com>
2022-09-27 00:14:27 +0200

    java learning

--- w1/genericsEx/Ex5.java
+++ w1/genericsEx/Ex5.java
@@ -1,7 +1,19 @@
 package w1.genericsEx;
 
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ex5 {
     public static void main(String[] args) {
-        System.out.println(Math.sin(Math.toRadians(90)));
+        List<Integer> list = new ArrayList<>(List.of(1,2,3,4,56,7));
+
+
+    }
+    static <T> T maxValue(List<? extends Number> list, int startSearch, int endSearch){
+        Integer result = 0;
+        list.max
+
+        return T;
     }
 }
