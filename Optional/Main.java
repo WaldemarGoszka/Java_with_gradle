9f6853facf592719e935a097dbf425209b1d8dcb
Waldemar Goszka Surface Pro <goszka.waldemar@gmail.com>
2022-10-06 10:57:20 +0200

    firt commit

--- Optional/Main.java
+++ Optional/Main.java
@@ -2,6 +2,7 @@ package Optional;
 
 import javax.swing.text.html.Option;
 import java.util.Optional;
+import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,4 +11,5 @@ public class Main {
         Optional<String> str = Optional.empty();
         Optional<Boolean> me = str.map(a -> a.contains("me hh"));
         System.out.println(me);
+        Function
     }}
