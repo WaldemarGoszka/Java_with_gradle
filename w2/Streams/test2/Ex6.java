f024af7363be070a98d39ca6356e2413b614b93c
WaldemarGoszka <goszka.waldemar@gmail.com>
2022-10-18 21:13:17 +0200

    java learning

---
+++ w2/Streams/test2/Ex6.java
@@ -0,0 +1,16 @@
+package w2.Streams.test2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class Ex6 {
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("Szczecin", "Zakopane", "Gdynia");
+        String result = strings.stream().reduce("Cities: ", (left, right) -> right);
+        System.out.println(result);
+        System.out.println(strings.stream().filter(a -> a.length()>7).collect(Collectors.joining(";"))+ ";");
+        Optional.of("").ifPresent(System.out::println);
+    }
+}
