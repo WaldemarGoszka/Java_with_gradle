98397be761c019dc5198a87da4ec10f2ad2cc399
WaldemarGoszka <goszka.waldemar@gmail.com>
2022-10-19 21:23:05 +0200

    java learning

---
+++ w2/Streams/Excesise/Ex1.java
@@ -0,0 +1,11 @@
+package w2.Streams.Excesise;
+
+import java.util.stream.Stream;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        //Napisz funkcyjną implementacje silni
+        Stream<Integer> stream = Stream.generate();
+
+    }
+}
