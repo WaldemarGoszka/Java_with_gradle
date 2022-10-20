1a1b79ee45731e2f33e0569f7b7743240eebc476
Waldemar Goszka <you@example.com>
2022-10-20 05:05:01 +0200

    java

--- w2/Streams/Excesise/Ex1.java
+++ w2/Streams/Excesise/Ex1.java
@@ -1,11 +1,20 @@
 package w2.Streams.Excesise;
 
+import java.math.BigInteger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Ex1 {
     public static void main(String[] args) {
         //Napisz funkcyjną implementacje silni
-        Stream<Integer> stream = Stream.generate();
+        Integer fractional = calculateFractional(5);
+        System.out.println(fractional);
+
+    }
+
+    private static Integer calculateFractional(int i) {
+        return IntStream.rangeClosed(1,i).reduce(1,(a,b) -> a * b);
 
     }
 }
---
+++ w2/Streams/Excesise/Ex5.java
@@ -0,0 +1,22 @@
+package w2.Streams.Excesise;
+
+import java.util.Collection;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class Ex5 {
+    public static void main(String[] args) {
+        String str = "nieein";
+        String noWhite = str.replace(" ", "");
+        System.out.println("IsPalindrom:" + isPalindrom(noWhite));
+    }
+
+    private static Boolean isPalindrom(String noWhite) {
+        String reverted = noWhite
+                .chars()
+                .mapToObj(c -> String.valueOf( (char)c))
+                .reduce("", (a,b)-> a+b);
+        return reverted.equals(noWhite);
+    }
+}
+
