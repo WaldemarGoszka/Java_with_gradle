22d9e331d4c03f2b952adb36d634429ac98cd460
Waldemar <goszka.waldemar@gmail.com>
2022-11-19 17:31:47 +0100

    java

--- src/zajavka/_warsztaty/w3/Main.java
+++ src/zajavka/_warsztaty/w3/Main.java
@@ -1,7 +1,25 @@
 package zajavka._warsztaty.w3;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("some");
+    Stream<String> lines = Files.lines();
+
+try (BufferedReader reader = Files.newBufferedReader(readingPath)) {
+        String line = null;
+        while((line = reader.readLine()) != null) {
+            System.out.println("nextLine: " + line); ①
+        }
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+    Path writingPath = Paths.get("/zajavka/bufferWritingFile.txt");
+    List<String> data = new ArrayList<>();
+try (BufferedWriter writer = Files.newBufferedWriter(writingPath)) {
+        writer.write("zajavka!"); ②
+    } catch (IOException e) {
+        e.printStackTrace();
     }
 }
