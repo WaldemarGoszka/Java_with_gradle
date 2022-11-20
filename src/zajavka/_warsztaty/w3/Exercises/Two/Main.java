0aafcc3b050e7f54f302dd2adef63ad4ac4c72a8
Waldemar Goszka Surface Pro <goszka.waldemar@gmail.com>
2022-11-20 04:55:35 +0100

    learn commit

---
+++ src/zajavka/_warsztaty/w3/Exercises/Two/Main.java
@@ -0,0 +1,10 @@
+package zajavka._warsztaty.w3.Exercises.Two;
+
+public class Main {
+    public static void main(String[] args) {
+        /*Wypisz na ekranie ścieżki wszystkich plików które zostały znalezione w podanej przez Ciebie
+ścieżce. Uwzględnij tylko te pliki tekstowe, które zawierają w swojej treści słowo zajavka. Dodaj
+możliwość podania rozszerzeń plików do pominięcia, gdyż niektóre z nich, np. .docx powodują błędy
+przy odczycie. Uwzględnij wszystkie pliki w katalogach zagnieżdżonych.*/
+    }
+}
