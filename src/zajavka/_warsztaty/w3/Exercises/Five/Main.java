f6a96ee36dbafe1b303d515dbc63b29efa01de0d
WaldemarGoszka <goszka.waldemar@gmail.com>
2022-11-22 17:43:35 +0100

    java learning

--- src/zajavka/_warsztaty/w3/Exercises/Five/Main.java
+++ src/zajavka/_warsztaty/w3/Exercises/Five/Main.java
@@ -1,13 +1,18 @@
 package zajavka._warsztaty.w3.Exercises.Five;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+
 
 public class Main {
     //Napisz program, który znajdzie drugie najdłuższe słowo i drugie słowo o długości 3 w podanym
     //przez Ciebie pliku. Do wygenerowania zawartości pliku wykorzystaj https://pl.lipsum.com/.
     public static void main(String[] args) {
-        Paths.get("src/zajavka/_warsztaty/w3/Exercises/Five/lorem.txt");
-
+        Path path = Paths.get("src/zajavka/_warsztaty/w3/Exercises/Five/lorem.txt");
+        Files.lines().
+                map(line -> line.replace(",", "").replace(".","").split(" "))
+                .toMap
 
     }
 }
