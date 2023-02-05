6f5576f038da89e0698cd07259bade6484046f5d
WaldemarGoszka <105630599+WaldemarGoszka@users.noreply.github.com>
2023-02-05 23:07:53 +0100

    java

--- src/main/java/pl/zajavka/_warsztaty/Main.java
+++ src/main/java/pl/zajavka/_warsztaty/Main.java
@@ -11,7 +11,7 @@ import java.util.stream.Stream;
 
 public class Main implements Interf {
     public static void main(String[] args) {
-        System.out.println("menu4");
+        System.out.println("menu45");
 
     }
 }
