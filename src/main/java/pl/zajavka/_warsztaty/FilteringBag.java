28537c712b856df33dec6a8d9f6b3e47f24cf5f5
WaldemarGoszka <105630599+WaldemarGoszka@users.noreply.github.com>
2023-02-11 09:37:29 +0100

    java

---
+++ src/main/java/pl/zajavka/_warsztaty/FilteringBag.java
@@ -0,0 +1,17 @@
+package pl.zajavka._warsztaty;
+
+import lombok.Value;
+
+import java.util.function.Predicate;
+
+@Value
+public class FilteringBag<T ,U extends Predicate<T>, V extends Predicate<T>> {
+    private T element;
+    private final V filter1;
+    private final V filter2;
+
+    public FilteringBag(V filter1, V filter2) {
+        this.filter1 = filter1;
+        this.filter2 = filter2;
+    }
+}
