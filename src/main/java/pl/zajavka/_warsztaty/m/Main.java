5c7745a69d3df9313a18cdb52ab31d1240d1533b
WaldemarGoszka <105630599+WaldemarGoszka@users.noreply.github.com>
2023-02-27 22:26:02 +0100

    java

---
+++ src/main/java/pl/zajavka/_warsztaty/m/Main.java
@@ -0,0 +1,34 @@
+package pl.zajavka._warsztaty.m;
+
+public class Main {
+    public static void main(String[] args) {
+        int Fullcircle = (100 % 6);
+
+        int Counter = 0;
+        String LastMarker;
+        if ( ++Counter <= Fullcircle  ){
+            LastMarker = "red";
+            System.out.println(LastMarker);
+        }
+        if ( ++Counter <= Fullcircle  ){
+            LastMarker = "green";
+            System.out.println(LastMarker);
+        }
+        if ( ++Counter <= Fullcircle  ){
+            LastMarker = "blue";
+            System.out.println(LastMarker);
+        }
+        if ( ++Counter <= Fullcircle  ){
+            LastMarker = "black";
+            System.out.println(LastMarker);
+        }
+        if ( ++Counter <= Fullcircle  ){
+            LastMarker = "yellow";
+            System.out.println(LastMarker);
+        }
+        if ( ++Counter <= Fullcircle  ){
+            LastMarker = "brown";
+            System.out.println(LastMarker);
+        }
+    }
+}
